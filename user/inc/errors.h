759790db9e6d3c3cf64697dd768529bd3e2827d9
Prajwal Y <prajwal299@gmail.com>
2015-09-27 19:57:20 -0400

    Adding errors.h

---
+++ user/inc/errors.h
@@ -0,0 +1,15 @@
+/** @file errors.h
+ *  @brief This file defines the error codes for
+ *  different modules of the thread library
+ *
+ *  @author Rohit Upadhyaya (rjupadhy)
+ *  @author Prajwal Yadapadithaya (pyadapad)
+ */
+
+#ifndef _THREAD_ERRORS_H
+#define _THREAD_ERRROS_H
+
+#define ERR_INVAL -1
+#define ERR_BUSY -2
+
+#endif /* _MUTEX_TYPE_H */
