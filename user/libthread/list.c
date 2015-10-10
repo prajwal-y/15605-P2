30f9b652ed1625a0a53226ca4208fa3a14b468c2
Rohit Upadhyaya <rohitju07@gmail.com>
2015-10-09 22:21:41 -0400

    Documentaiton

--- user/libthread/list.c
+++ user/libthread/list.c
@@ -1,10 +1,32 @@
+/** @file list.c
+ *  @brief This function implements the list operations
+ *         for a doubly linked list.
+ *
+ *  @author Rohit Upadhyaya (rjupadhy)
+ *  @author Prajwal Yadapadithaya (pyadapad)
+ */
 #include<list.h>
 
+/** @brief initialize the list
+ *
+ *  Initialize the head by making it point to itself
+ *
+ *  @param head the head of the doubly linked circular list
+ */
 void init_head(list_head *head) {
     head->next = head;
     head->prev = head;
 }
 
+/** @brief add a node to the list
+ *
+ *  This is not thread safe and must be protected by a mutex
+ *  by the programmer.
+ *
+ *  @param new_node The new node to be implemented in the list
+ *  @param prev the node previous to the node being inserted
+ *  @param next the node next to the node being inserted
+ */
 void add_to_list(list_head *new_node, list_head *prev, 
                                list_head *next) {
     new_node->next = next;
@@ -13,19 +35,40 @@ void add_to_list(list_head *new_node, list_head *prev,
     next->prev = new_node;
 }
 
+/** @brief add a node to the tail of the list
+ *
+ *  @param new_node The new node to be implemented in the list
+ *  @param head The head of the list
+ */
 void add_to_tail(list_head *new_node, list_head *head) {
     add_to_list(new_node, head->prev, head);
 }
 
+/** @brief add a node to the head of the list
+ *
+ *  @param new_node The new node to be implemented in the list
+ *  @param head The head of the list
+ */
 void add_to_head(list_head *new_node, list_head *head) {
     add_to_list(new_node, head, head->next);
 }
 
+/** @brief delete an entry from the list 
+ *
+ *  Does not free space associated with this node
+ *
+ *  @param node node to be deleted
+ */
 void del_entry(list_head *node) {
     node->prev->next = node->next;
     node->next->prev = node->prev;
 }
 
+/** @brief get the first entry in the list
+ *
+ *  @param head the head of the list
+ *  @return the first node in the list
+ */
 list_head *get_first(list_head *head) {
     if (head->next == head) {
         return NULL;
--- user/libthread/rwlock.c
+++ user/libthread/rwlock.c
@@ -8,6 +8,7 @@
 #include <mutex.h>
 #include <rwlock.h>
 #define RWLOCK_FREE 2
+#define RWLOCK_INVALID -1
 
 /** @brief function to initialize a read-write lock
  *
@@ -27,8 +28,21 @@ int rwlock_init(rwlock_t *rwlock) {
     return 0;
 }
 
+/** @brief lock the read write lock
+ *
+ *  If the lock requested is a write lock we increment the number of writers
+ *  and check if we can get the lock. We can get the lock either if the lock 
+ *  is currently free or if there are no readers currently reading the state 
+ *  being locked. Increasing the number of writers enures that future readers
+ *  and writers wait till the writers are done. Any read lock has to wait when
+ *  the lock is in write mode or the number of writers is greater than 0.
+ *
+ *  @param rwlock the rwlock to lock
+ *  @param type the type of lock being requested
+ *  @return void
+ */
 void rwlock_lock(rwlock_t *rwlock, int type) {
-    if (rwlock == NULL || type < 0 || type > 1) {
+    if (rwlock == NULL || (type != RWLOCK_READ && type != RWLOCK_WRITE)) {
         return;
     }
     mutex_lock(&rwlock->mutex);
@@ -49,6 +63,20 @@ void rwlock_lock(rwlock_t *rwlock, int type) {
     mutex_unlock(&rwlock->mutex);
 }
 
+/** @brief unlock the read write lock
+ *
+ *  If the lock is currently held by a writer and is unlocked we reduce 
+ *  the number of writers and set the type to free. We also broadcast to
+ *  all readers and signal any thread waiting for a write lock. If there
+ *  are other writers waiting for the lock readers promptly go back to sleep
+ *  and the signal ensures atleast one writer is awoken if present. If a read
+ *  lock is being unlocked, we reduce the number of readers and again broadcast 
+ *  to all readers and signal a writer. We broadcast to all readers since we 
+ *  allow multiple readers to acquire the lock.
+ *
+ *  @param rwlock the rwlock to unlock
+ *  @return void
+ */
 void rwlock_unlock(rwlock_t *rwlock) {
     if (rwlock == NULL) {
         return;
@@ -71,11 +99,30 @@ void rwlock_unlock(rwlock_t *rwlock) {
     mutex_unlock(&rwlock->mutex);
 }
 
+/** @brief destroy the read write lock
+ *
+ *  Invalidate the lock by setting the type to an invalid value.
+ *
+ *  @param rwlock the rwlock to unlock
+ *  @return void
+ */
 void rwlock_destroy(rwlock_t *rwlock ) {
-    rwlock->type = -1;
+    rwlock->type = RWLOCK_INVALID;
     return;
 }
 
+/** @brief downgrade the read write lock
+ *
+ *  This function does nothing if the lock is currently held by a reader.
+ *  If it is currently held by a writer we reduce the number of writers 
+ *  and broadcast to all readers and make the lock as read. This ensures 
+ *  that all the readers currently waiting and the calling thread acquire
+ *  the read lock. Future writers have to wait for these readers to release
+ *  the lock.
+ *
+ *  @param rwlock the rwlock to unlock
+ *  @return void
+ */
 void rwlock_downgrade(rwlock_t *rwlock) {
     if (rwlock == NULL) {
         return;
